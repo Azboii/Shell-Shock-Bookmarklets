f246fa740e09a962ca43f55874e532ef695ba9df
Celesteisscool <105241545+Azboii@users.noreply.github.com>
2023-04-28 09:48:01 -0400

    Update BetterGameUi.js

--- BetterGameUi.js
+++ BetterGameUi.js
@@ -4,14 +4,21 @@ var s = document.createElement("script");
 s.type = "text/javascript";
 var code = 'function yourFunction() {
 
+//In Menu
+document.getElementsByClassName("social-icons box_absolute")[0].style.display = "none";
+document.getElementById("gameDescription").style.display = "none";
+document.getElementsByClassName("vip-club-cta")[0].style.display = "none";
+
+
+//In Game
 document.getElementsByClassName("chw-progress-bar-msg box_aboslute centered nospace text-center fullwidth chw-msg chw-p-msg text-shadow-black-40")[0].innerhtml = "";
 document.getElementsByClassName("centered_y chw-egg-bundle")[0].style.display = "none";
 
 document.getElementsByClassName("double-eggs f_row align-items-center")[0].style.display = "none"; //this might change
 
-//Comment these lines if you want to show chat
-document.getElementsByClassName("chat-container")[0].style.display = "none";
-document.getElementsByClassName("chat-wrapper pause-ui-element box_absolute roundme_lg")[0].style.display = "none";
+//Uncomment these lines if you want to hide chat
+//document.getElementsByClassName("chat-container")[0].style.display = "none";
+//document.getElementsByClassName("chat-wrapper pause-ui-element box_absolute roundme_lg")[0].style.display = "none";
 
 //Weapons to show
 var weaponmaindiv = document.getElementsByClassName("display-grid grid-auto-flow-column justify-content-around gap-sm")[0];
@@ -23,7 +30,7 @@ var weapondivs = weaponmaindiv.getElementsByTagName("div");
 //weapondivs[1].style.display = "none"; //Scrambler
 weapondivs[2].style.display = "none"; //Free Ranger
 weapondivs[3].style.display = "none"; //RPEGG
-//weapondivs[4].style.display = "none"; //Whipper
+weapondivs[4].style.display = "none"; //Whipper
 //weapondivs[5].style.display = "none"; //Crackshot
 weapondivs[6].style.display = "none"; //Tri-Hard
 
